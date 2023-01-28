d8c8523df7c11c08c73d0c19160f141facb510e6
Maida <maida.maxime@gmail.com>
2023-01-28 02:34:10 +0100

    Add users from front-end to DB OK

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,32 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     Axios.get("http://localhost:3000/getUsers").then((response) => {
       setListOfUsers(response.data);
     });
   }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3000/createUser", {
+      name,
+      age,
+      username,
+    }).then((response) => {
+      setListOfUsers([
+        ...listOfUsers,
+        {
+          name,
+          age,
+          username,
+        },
+      ]);
+    });
+  };
 
   return (
     <div className="App">
@@ -22,6 +42,32 @@ function App() {
             </div>
           );
         })}
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          onChange={(e) => {
+            setAge(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button onClick={createUser} type="submit">
+          Ajouter un utilisateur
+        </button>
       </div>
     </div>
   );
